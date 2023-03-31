368226929460357bc504c101e8fb5a7a3ee09c7d
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-03-31 12:10:08 +0200

    made the photos dynamic

--- single-films.php
+++ single-films.php
@@ -9,9 +9,11 @@ if (have_posts()) : ?>
                 <h1 style="background-color: blue;"><?php the_title(); ?></h1>
                 <div>
                     <?php the_content(); ?>
+                    <?php the_post_thumbnail('medium_large', array('class' => 'test-class')); ?>
                 </div>
                 <div>
                     <div>Single Film</div>
+                    <?php get_template_part("parts/shared/post", "gallery"); ?>
                 </div>
             </div>
         </div>
