937c246ddfff3579b8485943b763a5d5ebca920e
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-04-03 11:44:50 +0200

    updated about and joannas page

--- page-about.php
+++ page-about.php
@@ -1,5 +1,32 @@
 <?php get_header(); ?>
 <section class="about">
-    <h1>this is the about page</h1>
+
+    <h2><?php echo the_title(); ?></h2>
+    <p><?php echo the_content(); ?></p>
+
+    <article>
+        <h3><?php echo "Bo Persson"; ?></h3>
+        <p><?php echo the_field('about_bo_persson'); ?></p>
+        <ul>
+            <li><?php echo the_field('bo_email'); ?></li>
+            <li><?php echo the_field('bo_phone'); ?></li>
+        </ul>
+        <div class="portrait">
+            <img src=<?php the_field('bo_photo'); ?> alt="">
+        </div>
+    </article>
+
+    <article>
+        <div class="portrait">
+            <img src=<?php the_field('joanna_photo'); ?> alt="">
+        </div>
+        <h3><?php echo "Joanna Helander"; ?></h3>
+        <p><?php echo the_field('about_joanna_helander'); ?></p>
+        <ul>
+            <li><?php echo the_field('joanna_email'); ?></li>
+            <li><?php echo the_field('joanna_phone'); ?></li>
+        </ul>
+
+    </article>
 </section>
 <?php get_footer(); ?>
--- page-joanna-helander.php
+++ page-joanna-helander.php
@@ -1,5 +1,30 @@
 <?php get_header(); ?>
 <section class="joanna">
-    <h1>this is Joannas page</h1>
+    <h2>Joanna Helander</h2>
+    <p><?php the_content(); ?></p>
+    <?php the_post_thumbnail() ?>
+    <strong><?php the_field("quote"); ?></strong>
+    <span>
+        <p>Johanna Helander</p>
+        <p><?php the_field("quote_source"); ?></p>
+    </span>
+
+    <div>
+        <h3>Work of Joanna Helander</h3>
+        <div>
+            <button>Photos</button>
+            <button>Films</button>
+            <button>Books</button>
+        </div>
+    </div>
+</section>
+
+<section>
+    <h4>Biography</h4>
+    <p><?php the_field("biography_part_1"); ?></p>
+    <p><?php the_field("biography_part_2"); ?></p>
+    <p><?php the_field("biography_part_3"); ?></p>
+    <img src=<?php the_field("image_2"); ?> alt="">
+    <img src=<?php the_field("image_2"); ?> alt="">
 </section>
 <?php get_footer(); ?>
