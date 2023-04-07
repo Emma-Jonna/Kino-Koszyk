f272a87e62c380287ab45c2bda81a95255f498ab
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-04-07 13:51:46 +0200

    can now get photos based on category

--- page-photos.php
+++ page-photos.php
@@ -123,7 +123,52 @@ $latest_posts_query = new WP_Query($args);
             </div>
         </div>
     <?php endif; ?>
-
 </section>
+
+<?php
+$args = array(
+    "post_type" => "photos",
+    "orderby" => "date",
+    "order" => "DESC",
+    "cat" => 11,
+    // 'taxonomy' => 'dining-category',
+);
+$query = new WP_Query($args);
+$posts = get_posts($query);
+
+// $cats = get_categories();
+/* foreach ($cats as $cat) {
+?>
+    <a href="<?php echo get_category_link($cat->term_id) ?>">
+        <?php echo $cat->name; ?>
+    </a>
+<?php
+} */
+
+// print_a(get_categories());
+/* ?>
+<a href=<?php get_category_link(11) ?>>America</a>
+<?php */
+
+// $posts = get_posts($args);
+// print_a(get_posts($query));
+// print_a(get_category_link(11));
+
+while ($query->have_posts()) : $query->the_post();
+    // get_template_part("parts/shared/post", "photos");
+
+    foreach ($posts as $key => $value) {
+        // print_a($value);
+        // print_a(get_category($value->ID));
+        // print_a($value->ID);
+        // var_dump(get_posts());
+        // print_a(get_the_tags());
+        /* print_a(get_the_tags($value->ID));
+        $tag_list = $tags = get_the_term_list($value->ID, 'custom-tag', '<ul class="snpt-tags"><li>', '</li><li>', '</li></ul>');
+        print $tag_list; */
+        // print_a(get_post($query));
+    }
+endwhile;
+?>
 <script src="<?= get_template_directory_uri() ?>/dropdown.js"></script>
 <?php get_footer(); ?>
